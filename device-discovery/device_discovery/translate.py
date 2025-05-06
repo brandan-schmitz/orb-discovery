f1f180416e7b05b0440fc40471e4de73dc22d52f
Brandan Schmitz <brandan.schmitz@celestialdata.net>
2025-05-06 17:28:31 -0500

    Different logging

--- device-discovery/device_discovery/translate.py
+++ device-discovery/device_discovery/translate.py
@@ -361,6 +361,8 @@ def translate_data(data: dict) -> Iterable[Entity]:
             
             access_vlan = get_or_create_vlan(access_vlan_id) if access_vlan_id is not None else None
             
+            logger.infp(f"Interface {if_name} on device {device.name} is in {port_mode} mode.")
+            
             if port_mode == "access":
                 matching_interface.mode = "access"
                 if access_vlan is not None:
