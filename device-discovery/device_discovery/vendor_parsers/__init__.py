e66e97a8b706a4a9d978bd2765e05e11f1aad9a8
Brandan Schmitz <brandan.schmitz@celestialdata.net>
2025-05-07 16:25:09 -0500

    Fix get_vendor_parser

--- device-discovery/device_discovery/vendor_parsers/__init__.py
+++ device-discovery/device_discovery/vendor_parsers/__init__.py
@@ -5,7 +5,6 @@ from typing import Type
 from device_discovery.vendor_parsers.base import VendorParser
 
 def get_vendor_parser(name: str) -> Type[VendorParser]:
-    
     if not (isinstance(name, str) and len(name) > 0):
         raise Exception("Please provide a valid parser name.")
     
@@ -13,32 +12,13 @@ def get_vendor_parser(name: str) -> Type[VendorParser]:
     # Try to not raise error when users requests IOS-XR for e.g.
     name = name.lower().replace("-", "")
     
-    for module_name in [
-        name
-    ]:
-        try:
-            module = importlib.import_module(module_name)
-            break
-        except ImportError as e:
-            message = str(e)
-            if "No module named" in message:
-                failed_module = message.split()[-1]
-                if failed_module.replace("'", "") in module_name:
-                    continue
-            raise e
-    else:
-        raise Exception(
-            'Cannot import "{install_name}". Is the library installed?'.format(
-                install_name=name
-            )
-        )
-    
+    try:
+        module = importlib.import_module(f"device_discovery.vendor_parsers.{name}")
+    except ImportError as e:
+        raise ImportError(f'Cannot import module device_discovery.vendor_parsers.{name}')
+
     for name, obj in inspect.getmembers(module):
         if inspect.isclass(obj) and issubclass(obj, VendorParser):
             return obj
     
-    raise Exception(
-        'No class inheriting "device_discovery.vendor_parsers.base.VendorParser" found in "{install_name}".'.format(
-            install_name=name
-        )
-    )
+    raise LookupError(f'No subclass of VendorParsers found in device_discovery.vendor_parsers.{name}')
