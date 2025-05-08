c1302ba7332c199bd57511e1f0bf1fdd5ef6ef14
Brandan Schmitz <brandan.schmitz@celestialdata.net>
2025-05-08 12:00:31 -0500

    Remove junos parser

--- device-discovery/device_discovery/vendor_parsers/junos.py
+++
@@ -1,7 +0,0 @@
-from napalm.junos import JunOSDriver
-from device_discovery.vendor_parsers.base import VendorParser
-
-class JuneOSParser(VendorParser):
-
-    def test():
-        print("Test method")
