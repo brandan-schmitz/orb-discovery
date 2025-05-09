4ed7113d2076c7575ae766653fc7b8dd8eb71223
Brandan Schmitz <brandan.schmitz@celestialdata.net>
2025-05-09 15:54:33 -0500

    Logging

--- device-discovery/device_discovery/translate.py
+++ device-discovery/device_discovery/translate.py
@@ -379,11 +379,17 @@ def translate_data(data: dict) -> Iterable[Entity]:
         if data.get("driver") == "junos":
             for i in range(len(entities) - 1, -1, -1):
                 entity: pb.Entity = entities[i]
-                interface_name = entity.interface.name
-                logger.info(f"Interface Name: {interface_name}")
-                
-                if '.' in interface_name:
-                    entities.pop(i)
+                if entity.interface:
+                    interface_name = entity.interface.name
+                    print(f"Checking interface: {interface_name}")
+                    
+                    if '.' in interface_name:
+                        entities.pop(i)
+                        print(f"Removed entity with interface: {interface_name}")
+
+            for entity in entities:
+                if entity.interface:
+                    print(f"Remaining entity with interface: {entity.interface.name}")
         
     except Exception as e:
         logger.error("Error caught in translate_data(): ", exc_info=True)
